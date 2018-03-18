eff28ef6c028fae240c215dbdcff8fada2a0bcee
coin-coop <coin_coop@yahoo.com>
2018-03-18 13:17:38 +0100

    Update client for fetching trades in given date range

--- src/main/java/org/coincoop/kucointradeexport/controller/client/CustomClient.java
+++ src/main/java/org/coincoop/kucointradeexport/controller/client/CustomClient.java
@@ -13,4 +13,10 @@ public interface CustomClient {
     JsonObject getUserInfo() throws IOException;
 
     List<JsonObject> getTradingHistory() throws IOException;
+
+    List<JsonObject> getTradingHistory(Long before, Long since) throws IOException;
+
+    List<JsonObject> getTradingHistory(String symbol) throws IOException;
+
+    List<JsonObject> getTradingHistory(String symbol, Long before, Long since) throws IOException;
 }
